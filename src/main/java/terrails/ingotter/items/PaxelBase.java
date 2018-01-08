d2502839c36f8378bad05be8bcb3dfa424f2743d
Terrails <terrails.business@outlook.com>
2018-01-09 00:17:15 +0100

    Paxel Improvement

--- src/main/java/terrails/ingotter/items/PaxelBase.java
+++ src/main/java/terrails/ingotter/items/PaxelBase.java
@@ -4,53 +4,119 @@ import com.google.common.collect.Sets;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTool;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.*;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.oredict.OreDictionary;
 import terrails.ingotter.Constants;
 
 import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PaxelBase extends ItemTool {
 
+    private static Set<Block> EFFECTIVE_ON = null;
+    
     public PaxelBase(ToolMaterial materialIn, String name) {
         super(2.5F, -2.0F, materialIn, Sets.newHashSet());
-        efficiency = 2.5F + materialIn.getEfficiency();
         setCreativeTab(Constants.TOOLS_TAB);
         setRegistryName(name);
         setUnlocalizedName(name);
+        if (EFFECTIVE_ON == null) {
+            EFFECTIVE_ON = getEffectiveOn();
+        }
     }
 
     @Override
     public float getDestroySpeed(@Nullable ItemStack stack, IBlockState state) {
-        return state.getBlock() == Blocks.OBSIDIAN ? 0.8F : super.getDestroySpeed(stack, state);
+        for (Block block : EFFECTIVE_ON) {
+            if (block == state.getBlock())
+                return this.efficiency;
+        }
+
+        Material material = state.getMaterial();
+        boolean axe = material != Material.WOOD && material != Material.PLANTS && material != Material.VINE;
+        boolean pickaxe = material != Material.IRON && material != Material.ANVIL && material != Material.ROCK;
+
+        return axe && pickaxe ? super.getDestroySpeed(stack, state) : this.efficiency;
+    }
+
+    private static Set<Block> getEffectiveOn() {
+        try {
+            // Runs only once so not that big of a performance hit
+            Set<Block> set = Sets.newHashSet();
+            String[] fieldNames = new String[]{"EFFECTIVE_ON", "field_150915_c", "field_150916_c", "field_150917_c"};
+            set.addAll((Set<Block>) ReflectionHelper.findField(ItemSpade.class, fieldNames).get(Sets.newHashSet()));
+            set.addAll((Set<Block>) ReflectionHelper.findField(ItemPickaxe.class, fieldNames).get(Sets.newHashSet()));
+            set.addAll((Set<Block>) ReflectionHelper.findField(ItemAxe.class, fieldNames).get(Sets.newHashSet()));
+            return set;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        ItemStack itemstack = player.getHeldItem(hand);
+
+        if (!player.canPlayerEdit(pos.offset(facing), facing, itemstack)) {
+            return EnumActionResult.FAIL;
+        } else {
+            IBlockState iblockstate = worldIn.getBlockState(pos);
+            Block block = iblockstate.getBlock();
+
+            if (facing != EnumFacing.DOWN && worldIn.getBlockState(pos.up()).getMaterial() == Material.AIR && block == Blocks.GRASS) {
+                IBlockState iblockstate1 = Blocks.GRASS_PATH.getDefaultState();
+                worldIn.playSound(player, pos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+                if (!worldIn.isRemote) {
+                    worldIn.setBlockState(pos, iblockstate1, 11);
+                    itemstack.damageItem(1, player);
+                }
+
+                return EnumActionResult.SUCCESS;
+            } else {
+                return EnumActionResult.PASS;
+            }
+        }
     }
 
     @Override
     public boolean canHarvestBlock(IBlockState state) {
         Block block = state.getBlock();
 
+        if (block == Blocks.SNOW_LAYER)
+            return true;
+        else if (block == Blocks.SNOW)
+            return true;
+
         if (block == Blocks.OBSIDIAN) {
-            return toolMaterial.getHarvestLevel() == 3;
-        } else if (block == Blocks.DIAMOND_BLOCK || block == Blocks.DIAMOND_ORE) {
-            return toolMaterial.getHarvestLevel() >= 2;
-        } else if (block == Blocks.GOLD_BLOCK || block == Blocks.GOLD_ORE) {
-            return toolMaterial.getHarvestLevel() >= 2;
-        } else if (block == Blocks.IRON_BLOCK || block == Blocks.IRON_ORE) {
-            return toolMaterial.getHarvestLevel() >= 1;
-        } else if (block == Blocks.LAPIS_BLOCK || block == Blocks.LAPIS_ORE) {
-            return toolMaterial.getHarvestLevel() >= 1;
-        } else if (block == Blocks.REDSTONE_ORE || block == Blocks.LIT_REDSTONE_ORE) {
-            return toolMaterial.getHarvestLevel() >= 2;
-        } else if (block == Blocks.ANVIL) {
-            return toolMaterial.getHarvestLevel() >= 0;
-        } else if (block == Blocks.SNOW || block == Blocks.SNOW_LAYER) {
-            return true;
-        } else if (state.getMaterial() == Material.ROCK) {
-            return true;
-        } else return state.getMaterial() == Material.IRON;
+            return this.toolMaterial.getHarvestLevel() == 3;
+        } else if (block != Blocks.DIAMOND_BLOCK && block != Blocks.DIAMOND_ORE) {
+            if (block != Blocks.EMERALD_ORE && block != Blocks.EMERALD_BLOCK) {
+                if (block != Blocks.GOLD_BLOCK && block != Blocks.GOLD_ORE) {
+                    if (block != Blocks.IRON_BLOCK && block != Blocks.IRON_ORE) {
+                        if (block != Blocks.LAPIS_BLOCK && block != Blocks.LAPIS_ORE) {
+                            if (block != Blocks.REDSTONE_ORE && block != Blocks.LIT_REDSTONE_ORE) {
+                                Material material = state.getMaterial();
+                                return material == Material.ROCK || material == Material.IRON || material == Material.ANVIL;
+                            } else return this.toolMaterial.getHarvestLevel() >= 2;
+                        } else return this.toolMaterial.getHarvestLevel() >= 1;
+                    } else return this.toolMaterial.getHarvestLevel() >= 1;
+                } else return this.toolMaterial.getHarvestLevel() >= 2;
+            } else return this.toolMaterial.getHarvestLevel() >= 2;
+        } else return this.toolMaterial.getHarvestLevel() >= 2;
     }
 
     @Override
